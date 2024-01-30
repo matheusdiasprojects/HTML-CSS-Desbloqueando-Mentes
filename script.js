22c1d02b3004a0a90e7f12d0f1b65d147218f8cf
Matheus Dias <86936124+matheusdiasprojects@users.noreply.github.com>
2024-01-30 15:58:05 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -9,3 +9,22 @@ cards.forEach((item) => {
     item.classList.add("active");
   });
 });
+
+/* noticia */
+window.addEventListener('scroll', function () {
+  const scrollPosition = window.scrollY;
+
+  // Cor inicial e final
+  const initialColor = '#E52E71'; // Cor inicial
+  const finalColor = '#00BF63'; // Cor final
+
+  // Interpolação linear para calcular a cor intermediária
+  const r = Math.round(parseInt(initialColor.slice(1, 3), 16) * (1 - scrollPosition / window.innerHeight) + parseInt(finalColor.slice(1, 3), 16) * (scrollPosition / window.innerHeight));
+  const g = Math.round(parseInt(initialColor.slice(3, 5), 16) * (1 - scrollPosition / window.innerHeight) + parseInt(finalColor.slice(3, 5), 16) * (scrollPosition / window.innerHeight));
+  const b = Math.round(parseInt(initialColor.slice(5, 7), 16) * (1 - scrollPosition / window.innerHeight) + parseInt(finalColor.slice(5, 7), 16) * (scrollPosition / window.innerHeight));
+
+  const interpolatedColor = `rgb(${r}, ${g}, ${b})`;
+
+  document.querySelector('.noticia-trick').style.color = interpolatedColor;
+  document.querySelector('.noticia-trick').style.color = interpolatedColor;
+});
